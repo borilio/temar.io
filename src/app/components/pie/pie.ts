62d45be77b51962c71a05a58268515474ad29be2
borilio <exprimetuandroid@gmail.com>
2025-09-01 16:04:06 +0200

    Arreglado el cambio de footer por data en el config.json

--- src/app/components/pie/pie.ts
+++ src/app/components/pie/pie.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PRIMENG_IMPORTS } from '../../shared/primeng.imports';
-import { Config, Footer } from '../../models/config.model';
+import { Config, Data } from '../../models/config.model';
 import { ConfigService } from '../../services/config.service';
 
 @Component({
@@ -11,12 +11,12 @@ import { ConfigService } from '../../services/config.service';
 })
 export class Pie implements OnInit {
   // Objeto de configuración que incluye información del footer (entre otras)
-  public footer: Footer;
+  public footer: Data;
 
 
   constructor(private configService: ConfigService) {
     // Inicializamos la config
-    this.footer = configService.getFooter();
+    this.footer = configService.getData();
   }
 
 
